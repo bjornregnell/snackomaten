b1f6f37164f1ec864f56cdce5dd0dbe19acbf608
bjornregnell <bjorn.regnell@cs.lth.se>
2025-03-18 18:11:42 +0100

    add clean script

---
+++ clean.sh
@@ -0,0 +1,11 @@
+rm -rf .bsp .bloop .metals target .scala-build
+cd client
+rm -rf .bsp .bloop .metals target .scala-build
+cd ..
+cd server
+rm -rf .bsp .bloop .metals target .scala-build
+cd ..
+cd common
+rm -rf .bsp .bloop .metals target .scala-build
+cd ..
+
