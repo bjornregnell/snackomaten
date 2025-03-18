8e4886e39d61f47128f94bc3172c6417b653d5af
bjornregnell <bjorn.regnell@cs.lth.se>
2025-03-18 20:00:45 +0100

    handle client-main arguments

--- client/src/main/scala/client-main.scala
+++ client/src/main/scala/client-main.scala
@@ -1,6 +1,10 @@
 package snackomaten 
 
 @main def ClientMain(args: String*) = 
-  println("Hello snackomaten Client!     Ctrl+D to stop spamming")
-  val c = Client()
+  Terminal.put("Welcome to the snackomaten terminal client! Ctrl+D to stop spamming ? for help")
+  val c = 
+    if args.length == 1 then Client(host = args(0))
+    else if args.length == 2 && args(1).toIntOption.isDefined then Client(host = args(0), port = args(1).toInt) 
+    else Client()
+
   c.start()
