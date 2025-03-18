5a1063d7855bd4401da5d0a7773061ff997a44df
bjornregnell <bjorn.regnell@cs.lth.se>
2025-03-18 20:53:43 +0100

    remove Codec and Message as they are not used yet

--- common/src/main/scala/Message.scala
+++
@@ -1,8 +0,0 @@
-package snackomaten 
-
-case class Message(text: String):
-  def encode: Array[Byte] = Codec.encode(text)
-  def isValid: Boolean = true //TODO check that text has all valid parts
-
-object Message:
-    def decode(bytes: Array[Byte]): Message = Message(Codec.decode(bytes))
