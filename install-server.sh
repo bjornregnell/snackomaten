b8a32bcbf0d77ff7045c9b564b3ef5f249eb6fdd
bjornregnell <bjorn.regnell@cs.lth.se>
2025-03-19 16:50:31 +0100

    rename script

--- install-server.sh
+++
@@ -1,6 +0,0 @@
-VERSION=0.2.0
-SCALAVERSION=3.6.4
-mkdir -p ~/snackomaten
-mkdir -p ~/snackomaten/bin
-sbt --client "server/assembly"
-cp server/target/scala-$SCALAVERSION/snackomatenServer-assembly-$VERSION.jar ~/snackomaten/bin/snackomaten-server.jar
