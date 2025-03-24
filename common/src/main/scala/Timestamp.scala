79d76893c8084cde0b04b4b5611326af2a9a389f
bjornregnell <bjorn.regnell@cs.lth.se>
2025-03-24 20:21:13 +0100

    add Timestamp

---
+++ common/src/main/scala/Timestamp.scala
@@ -0,0 +1,20 @@
+package snackomaten
+
+import java.sql.Time
+
+case class Timestamp(underlying: java.time.Instant) extends Ordered[Timestamp]:
+
+  override def compare(that: Timestamp): Int = underlying.compareTo(that.underlying)
+
+  def show: String = underlying.toString
+
+  def toDate: java.util.Date = java.util.Date.from(underlying)
+
+object Timestamp:
+
+  def now(): Timestamp = Timestamp(java.time.Instant.now())
+  
+  def parse(s: String): Option[Timestamp] = 
+    try Some(Timestamp(java.time.Instant.parse(s))) catch case e: Throwable => None 
+
+end Timestamp
