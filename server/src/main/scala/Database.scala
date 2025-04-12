d34fa0355de7a9417ea2bad188acce77f9931708
bjornregnell <bjorn.regnell@cs.lth.se>
2025-04-12 13:30:09 +0200

    rm dummy code file

--- server/src/main/scala/Database.scala
+++
@@ -1,9 +0,0 @@
-package snackomaten
-
-object Database:
-  case class User(uid: String)
-  def loadUsers(): Concurrent.MutableKeyValueStore[String, User] = ???
-  def saveUsers(): Unit = ???
-  private val users: Concurrent.MutableKeyValueStore[String, User] = loadUsers()
-  def getUser(uid: String): User = ???
-  def updateUser(u: User): Unit = ???
