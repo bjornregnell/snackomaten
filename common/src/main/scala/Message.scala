05c6ccac2556da431f59b342294be2ce9d6997d3
bjornregnell <bjorn.regnell@cs.lth.se>
2025-04-13 13:25:52 +0200

    add encryption tag and encrypt the whole message

--- common/src/main/scala/Message.scala
+++ common/src/main/scala/Message.scala
@@ -1,29 +1,45 @@
 package snackomaten
 
 case class Message(userId: String, cmd: Message.Cmd, time: Timestamp, body: String):
-  def encode(pwd: String = "unsecurePassword"): String = 
+  def encode(secretOpt: Option[String] = Some("unsecurePassword")): String = 
     import Message.Key.*
-    UserId.withValue(userId) + ";" 
-    + Command.withValue(Message.Cmd.Send.toString) + ";" 
-    + Time.withValue(Timestamp.now().encode) + ";" 
-    + Body.withValue(Crypto.AES.encryptString(secret = body, password = pwd))
+    val msg = java.lang.StringBuilder()
+      .append(UserId.withValue(userId)).append(';')
+      .append(Command.withValue(Message.Cmd.Send.toString)).append(';')
+      .append(Time.withValue(Timestamp.now().encode)).append(';')
+      .append(Body.withValue(body)).append(';')
+    val et = Message.isEncryptedTag(secretOpt.isDefined)
+    if secretOpt.isEmpty 
+    then msg.insert(0, et).toString 
+    else Crypto.AES.encryptString(secret = msg.toString, password = secretOpt.get).prepended(et)
+  end encode
+
 
 object Message:
   def apply(userId: String, cmd: Cmd, body: String): Message = 
     new Message(userId: String, cmd: Cmd, Timestamp.now(), body: String)
 
-  enum DecodeError:
-    case InvalidUserId, InvalidCmd, InvalidTime, InvalidBody
+  def isEncryptedTag(isEncrypted: Boolean): Char = if isEncrypted then 'E' else 'C'
 
-  def decode(raw: String, pwd: String = "unsecurePassword"): Either[DecodeError, Message] = 
-    for
-      u <- raw.valueOf(Key.UserId).toRight(DecodeError.InvalidUserId)
-      c <- raw.valueOf(Key.Command).flatMap(Cmd.fromString).toRight(DecodeError.InvalidCmd)
-      t <- raw.valueOf(Key.Time).flatMap(Timestamp.decode).toRight(DecodeError.InvalidTime)
-      b <- raw.valueOf(Key.Body)
-            .flatMap(encrypted => Crypto.AES.decryptString(encrypted, pwd))
-            .toRight(DecodeError.InvalidBody)
-    yield Message(u, c, t, b)
+  enum DecodeError:
+    case InvalidMessage, DecryptionFailed, InvalidUserId, InvalidCmd, InvalidTime, InvalidBody
+
+  def decode(raw: String, secretOpt: Option[String] = Some("unsecurePassword")): Either[DecodeError, Message] = 
+    if raw.length < 2 then Left(DecodeError.InvalidMessage) 
+    else
+      val decoded: Option[String] = 
+        if raw(0) == Message.isEncryptedTag(false) then Some(raw.drop(1))
+        else if secretOpt.isDefined then Crypto.AES.decryptString(raw.drop(1), secretOpt.get)
+        else None
+      if decoded.isEmpty then Left(DecodeError.DecryptionFailed) 
+      else
+        for
+          u <- decoded.get.valueOf(Key.UserId).toRight(DecodeError.InvalidUserId)
+          c <- decoded.get.valueOf(Key.Command).flatMap(Cmd.fromString).toRight(DecodeError.InvalidCmd)
+          t <- decoded.get.valueOf(Key.Time).flatMap(Timestamp.decode).toRight(DecodeError.InvalidTime)
+          b <- decoded.get.valueOf(Key.Body).toRight(DecodeError.InvalidBody)
+        yield Message(u, c, t, b)
+  end decode
           
   enum Key:
     case UserId, Command, Time, Body
